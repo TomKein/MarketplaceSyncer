eeca474d3ca106888a386d8bdc5444048d19d714
Aleksey Rogachev <rogachev.aleksey@gmail.com>
2022-03-15 11:50:14 +0300

    добавил основные поля в выгрузку xml

--- Sites/YoulaXml.cs
+++ Sites/YoulaXml.cs
@@ -25,11 +25,31 @@ namespace Selen.Sites {
             //список карточек с положительным остатком и ценой, у которых есть ссылка на юлу
             var bus = _bus.Where(w => w.amount > 0 && w.price > 0 && w.youla.Contains("http"));
             //для каждой карточки
-            foreach (var b in bus) {
+            foreach (var b in bus.Take(5)) {
                 //id объявления на юле из ссылки
                 var youlaId = b.youla.Split('-').Last();
                 //элемент offer
                 var offer = new XElement("offer", new XAttribute("id", youlaId));
+                //категория товара
+                offer.Add(new XElement("youlaCategoryId", GetCategory()));
+                //подкатегория товара
+                offer.Add(new XElement("youlaSubcategoryId", GetSubCategory()));
+                //адрес магазина
+                offer.Add(new XElement("adress", "Россия, Калуга, Московская улица, 331"));
+                //цена
+                offer.Add(new XElement("price", b.price));
+                //телефон
+                offer.Add(new XElement("phone", "8 920 899-45-45"));
+                //наименование
+                offer.Add(new XElement("name", b.NameLength(100)));
+                //изображения - пока только первое
+                offer.Add(new XElement("picture", b.images.First().url));
+                //описание
+                offer.Add(new XElement("description", b.DescriptionList(3000).Aggregate((a1,a2)=>a1+"\r\n"+a2)));
+                //имя менеджера
+                offer.Add(new XElement("managerName", "Менеджер 1"));
+                
+
                 //добавляю элемент offer в контейнер offers
                 offers.Add(offer);
             }
@@ -37,9 +57,18 @@ namespace Selen.Sites {
             shop.Add(offers);
             //добавляю shop в корневой элемент yml_catalog
             root.Add(shop);
-            
+            //добавляю root в документ
+            xml.Add(root);
             //сохраняю файл
             xml.Save(filename);
+        }
+
+        private object GetCategory() {
+            throw new NotImplementedException();
+        }
+
+        private object GetSubCategory() {
+            throw new NotImplementedException();
         }
     }
 
