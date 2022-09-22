25b350fb20f16f9b05793138eeeb05ada680e5cf
Rogachev Aleksey <rogachev.aleksey@gmail.com>
2022-09-22 11:33:12 +0300

    ftp creditionals

--- Tools/SftpClient.cs
+++ Tools/SftpClient.cs
@@ -36,17 +36,19 @@ namespace Selen.Base {
         //выгрузка FTP
         public static async Task FtpUploadAsync(string fname) {
             System.Net.WebClient ftp = new System.Net.WebClient();
-            ftp.Credentials = new NetworkCredential("u1633438", "HIJv3W71Xmftd61D"); 
+            //ftp.Credentials = new NetworkCredential("u1633438", "HIJv3W71Xmftd61D"); 
+            ftp.Credentials = new NetworkCredential("u1723083", "0eCssGXW9fFXa39u");
             for (int f = 1; f <= 5; f++) {
                 try {
                     await Task.Factory.StartNew(() => {
-                        ftp.UploadFile(new Uri("ftp://31.31.198.99:21/www/avtotehnoshik.tk/" 
-                            + (fname.Contains("\\")? fname.Split('\\').Last():fname)), "STOR", fname);
+                        //ftp.UploadFile(new Uri("ftp://31.31.198.99:21/www/avtotehnoshik.tk/" 
+                        ftp.UploadFile(new Uri("ftp://37.140.192.251:21/www/avtotehnoshik.ru/"
+                            + (fname.Contains("\\") ? fname.Split('\\').Last() : fname)), "STOR", fname);
                     });
-                    Log.Add("SftpClient: "+fname+" - успешно отправлен!");
+                    Log.Add("SftpClient: " + fname + " - успешно отправлен!");
                     break;
                 } catch (Exception ex) {
-                    Log.Add("SftpClient: " + fname + " - ошибка отправки FTP (" + f + ") - " 
+                    Log.Add("SftpClient: " + fname + " - ошибка отправки FTP (" + f + ") - "
                         + ex.Message);
                     await Task.Delay(60000);
                 }
