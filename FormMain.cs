675828bb48c76111960ad07e33a1e10216b918d4
Rogachev Aleksey <rogachev.aleksey@gmail.com>
2023-09-29 11:32:30 +0300

    1.159 hotix

--- FormMain.cs
+++ FormMain.cs
@@ -16,7 +16,7 @@ using Selen.Base;
 
 namespace Selen {
     public partial class FormMain : Form {
-        string _version = "1.158";
+        string _version = "1.159";
 
         DB _db = new DB();
 
