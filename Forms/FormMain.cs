d0f247c134725153367bb473777684bdc94a1d5b
Алексей Рогачев <rogachev.aleksey@gmail.com>
2024-11-19 05:34:42 +0300

    +

--- Forms/FormMain.cs
+++ Forms/FormMain.cs
@@ -17,7 +17,7 @@ using System.Diagnostics;
 
 namespace Selen {
     public partial class FormMain : Form {
-        int _version = 209;
+        int _version = 210;
         //todo move this fields to class365api class
         YandexMarket _yandexMarket;
         VK _vk;
